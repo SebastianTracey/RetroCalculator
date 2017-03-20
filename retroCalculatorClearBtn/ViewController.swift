f787594231db3a2b97e7ccde1efa51eab8b50696
Sebastian Tracey <SebastianTracey@Sebastians-Air.lan>
2017-03-20 10:20:36 +0100

    added files for retroCalculator git status

--- retroCalculatorClearBtn/ViewController.swift
+++ retroCalculatorClearBtn/ViewController.swift
@@ -7,19 +7,125 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    var result = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: url)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(btn: UIButton!) {
+        playSound()
+        
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+        
     }
-
+    
+    @IBAction func onAddPressed(_ sender: Any) {
+        processOperation(op: Operation.Add)
+    }
+    @IBAction func onSubtractPressed(_ sender: Any) {
+        processOperation(op: Operation.Subtract)
+    }
+    @IBAction func onMultiplyPressed(_ sender: Any) {
+        processOperation(op: Operation.Multiply)
+    }
+    @IBAction func onDividePressed(_ sender: Any) {
+        processOperation(op: Operation.Divide)
+    }
+    @IBAction func onEqualPressed(_ sender: Any) {
+        processOperation(op: currentOperation)
+    }
+    @IBAction func onClearPressed(_ sender: Any) {
+        playSound()
+        
+        leftValStr = ""
+        rightValStr = ""
+        runningNumber = ""
+        outputLbl.text = "0"
+        currentOperation = Operation.Empty
+        
+        print("left = " + leftValStr)
+        print("rigth = " + rightValStr)
+        print("running + " + runningNumber)
+        print("current + " + "\(currentOperation)")
+    }
+    
+    func processOperation(op: Operation) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            
+            // run some math
+            // User selected a operater, but then selected another operater without first entering a number
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                } else if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLbl.text = result
+            }
+            
+            currentOperation = op
+        } else {
+        // This is the first time the oprater is pressed
+        
+        leftValStr = runningNumber
+        runningNumber = ""
+        currentOperation = op
+        }
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+            btnSound.play()
+    }
 
 }
 
